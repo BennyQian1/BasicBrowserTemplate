eae33601658a56f81df93f26ccf4bd2dfa1e1e23
BennyQian1 <tuq28313@temple.edu>
2025-03-27 12:09:36 -0400

    -Created a basic implementation of browser

--- app/src/main/java/edu/temple/basicbrowser/MainActivity.kt
+++ app/src/main/java/edu/temple/basicbrowser/MainActivity.kt
@@ -1,11 +1,18 @@
 package edu.temple.basicbrowser
 
-import androidx.appcompat.app.AppCompatActivity
+import android.annotation.SuppressLint
+import android.content.ActivityNotFoundException
+import android.content.Intent
+import android.net.ConnectivityManager
+import android.net.Uri
 import android.os.Bundle
 import android.webkit.WebView
 import android.webkit.WebViewClient
 import android.widget.EditText
 import android.widget.ImageButton
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+
 
 class MainActivity : AppCompatActivity() {
 
@@ -13,6 +20,7 @@ class MainActivity : AppCompatActivity() {
     private lateinit var goButton: ImageButton
     private lateinit var webView: WebView
 
+    @SuppressLint("SetJavaScriptEnabled")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -21,6 +29,8 @@ class MainActivity : AppCompatActivity() {
         goButton = findViewById(R.id.goButton)
         webView = findViewById(R.id.webView)
 
+        webView.settings.javaScriptEnabled = true
+
         // Allow your browser to intercept hyperlink clicks
         webView.webViewClient = object: WebViewClient() {
             override fun onPageFinished(view: WebView?, url: String?) {
@@ -28,5 +38,11 @@ class MainActivity : AppCompatActivity() {
             }
         }
 
+        goButton.setOnClickListener() {
+            var inputText: String = urlEditText.text.toString()
+            if (!inputText.startsWith("http://") && !inputText.startsWith("https://"))
+                inputText = "http://$inputText";
+            webView.loadUrl(inputText)
+        }
     }
 }
